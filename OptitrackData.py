744e6449321be42a5c24402f521079918a67526a
juoyangryan <juoyang@gatech.edu>
2019-11-15 11:48:45 -0500

    returns ending position of all balls

--- OptitrackData.py
+++ OptitrackData.py
@@ -4,12 +4,15 @@ file1 = open("optitrack.csv", "r")
 allLines = file1.readlines()
 allLines = allLines[8::]
 df_output = pd.DataFrame(columns = ['Ball #', 'Dist.'])
+count = 5
+outputRows = 0
 for item in allLines:
 	itemList = item.split(",")
 	#x:2 ; y: 3; z: 4; 
-	print(itemList[2])
-	if (itemList[2] != "" and -0.01 < float(itemList[2]) < 0.01):
-		df_output.loc[0] = [1, itemList[3]]
-		break
+	if (itemList[count] != "" and -0.01 < float(itemList[count]) < 0.01):
+		print(itemList[count])
+		df_output.loc[outputRows] = [outputRows + 1, itemList[count + 1]]
+		outputRows += 1
+		count += 4
 
 df_output.to_excel("positions.xlsx", sheet_name = 'temp', index = False)
