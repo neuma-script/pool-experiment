1b4e56fa6bda3ee7d1f4b2e29179d1a021c6ff64
juoyangryan <juoyang@gatech.edu>
2019-11-26 02:35:42 -0500

    new data file

--- OptitrackData.py
+++ OptitrackData.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 file1 = open("optitrack.csv", "r")
 allLines = file1.readlines()
-allLines = allLines[8::]
+allLines = allLines[7::]
 df_output = pd.DataFrame(columns = ['Ball #', 'Dist.'])
-count = 5
+count = 6
 outputRows = 0
 for item in allLines:
 	itemList = item.split(",")
 	#x:2 ; y: 3; z: 4; 
 	if (itemList[count] != "" and -0.01 < float(itemList[count]) < 0.01):
-		print(itemList[count])
-		df_output.loc[outputRows] = [outputRows + 1, itemList[count + 1]]
+		df_output.loc[outputRows] = [outputRows + 1, itemList[count + 2]]
 		outputRows += 1
 		count += 4
 
 df_output.to_excel("positions.xlsx", sheet_name = 'temp', index = False)
+print("complete")
