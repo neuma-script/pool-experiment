5257c7174364bed360c1242410e0da7e184f2a18
juoyangryan <juoyang@gatech.edu>
2019-12-03 02:04:17 -0500

    added proximity column

--- OptitrackData.py
+++ OptitrackData.py
@@ -3,7 +3,7 @@ import pandas as pd
 file1 = open("optitrack.csv", "r")
 allLines = file1.readlines()
 allLines = allLines[7::]
-df_output = pd.DataFrame(columns = ['Ball #', 'Dist.'])
+df_output = pd.DataFrame(columns = ['Ball #', 'Dist.', 'Proximity'])
 count = 6
 outputRows = 0
 for item in allLines:
@@ -12,7 +12,16 @@ for item in allLines:
 	if (count >= len(itemList)):
 		break
 	if (-0.01 < float(itemList[count]) < 0.01):
-		df_output.loc[outputRows] = [outputRows + 1, itemList[count + 2]]
+		if (float(itemList[count + 2]) < 1 and float(itemList[count + 2]) > -1):
+			proximity = "Hit"
+		elif (float(itemList[count + 2]) < 150 and float(itemList[count + 2]) > -150):
+			proximity = "Close"
+		elif (float(itemList[count + 2]) < 350 and float(itemList[count + 2]) > -350):
+			proximity = "Medium"
+		else:
+			proximity = "Off Target"
+
+		df_output.loc[outputRows] = [outputRows + 1, itemList[count + 2], proximity]
 		outputRows += 1
 		count += 7
 
